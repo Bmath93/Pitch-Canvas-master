61a1365eec9178073885bed2308cca49b058653c
Bmath93 <Bmath93@gmail.com>
2014-03-21 15:15:35 -0400

    installed VVOSC libraries; unknown error when creating new OSC message

--- Mobile Music Template/MAGTandemCanvas.m
+++ Mobile Music Template/MAGTandemCanvas.m
@@ -7,17 +7,38 @@
 //
 
 #import "MAGTandemCanvas.h"
-//#include "osc/OscOutboundPacketStream.h"
-#include "osc/UdpSocket.h"
+
+#import <VVOSC/VVOSC.h>
+#import <VVOSC/OSCManager.h>
+#import <VVOSC/OSCInPort.h>
+#import <VVOSC/OSCOutPort.h>
+#import <VVOSC/OSCMessage.h>
+
+#define ADDRESS "localhost"
+#define PORT 7000
+
+#define OUTPUT_BUFFER_SIZE 1024
 
 @implementation MAGTandemCanvas
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
 - (IBAction)backToSetup:(id)sender {
     [self.navigationController popToRootViewControllerAnimated:YES];
 }
+
 - (IBAction)sendOSCstuff:(id)sender {
+    OSCManager *manager = [[OSCManager alloc] init];
+    [manager setDelegate:self];
     
+    OSCOutPort *outport = [manager createNewOutputToAddress:@"localhost" atPort:7000];
+    
+    [manager createNewInputForPort:7000];
+    
+    //OSCMessage *newMsg = [OSCMessage createWithAddress:@"/Address/Path/1"];
 }
-
-
 
 @end
