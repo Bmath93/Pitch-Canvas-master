d7a45ce194f5923cdbb3b7905d25d34fb4054a7d
Bmath93 <Bmath93@gmail.com>
2014-03-24 15:48:56 -0400

    Merge branch 'master' of https://github.com/Bmath93/Pitch-Canvas-master
    Conflicts:
    Mobile Music Template/ExampleOscListener.cpp
    Mobile Music Template/ExampleOscListener.h
    Mobile Music Template/OscPacketListenerWrapper.mm

--- Mobile Music Template/MAGTandemCanvas.h
+++ Mobile Music Template/MAGTandemCanvas.h
@@ -8,9 +8,17 @@
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
+#import <VVOSC/VVOSC.h>
+#import <VVOSC/OSCManager.h>
+#import <VVOSC/OSCInPort.h>
+#import <VVOSC/OSCOutPort.h>
+#import <VVOSC/OSCMessage.h>
 
 @interface MAGTandemCanvas : UIViewController
+
 @property (weak, nonatomic) IBOutlet UILabel *UpLabel;
 @property (weak, nonatomic) IBOutlet UILabel *DownLabel;
+@property OSCManager *manager;
+@property OSCOutPort *outport;
 
 @end
--- Mobile Music Template/MAGTandemCanvas.m
+++ Mobile Music Template/MAGTandemCanvas.m
@@ -7,17 +7,36 @@
 //
 
 #import "MAGTandemCanvas.h"
-//#include "osc/OscOutboundPacketStream.h"
-#include "osc/UdpSocket.h"
+
+#import <VVOSC/VVOSC.h>
+#import <VVOSC/OSCManager.h>
+#import <VVOSC/OSCInPort.h>
+#import <VVOSC/OSCOutPort.h>
+#import <VVOSC/OSCMessage.h>
+
+#define ADDRESS "localhost"
+#define PORT 7000
+
+#define OUTPUT_BUFFER_SIZE 1024
 
 @implementation MAGTandemCanvas
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.manager = [[OSCManager alloc] init];
+    [self.manager setDelegate:self];
+    self.outport = [self.manager createNewOutputToAddress:@"localhost" atPort:7000];
+    [self.manager createNewInputForPort:7000];
+}
+
 - (IBAction)backToSetup:(id)sender {
     [self.navigationController popToRootViewControllerAnimated:YES];
 }
+
 - (IBAction)sendOSCstuff:(id)sender {
-    
+    //OSCMessage *newMsg = [[OSCMessage alloc] initWithAddress:@"empty"];
+    OSCMessage *newMsg = [OSCMessage createWithAddress:@"empty"];
 }
-
-
 
 @end
